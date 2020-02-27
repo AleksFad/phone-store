b232c6d4a78ad37117bc165bc4cde697088d53a3
AleksanderFade <aleksander.fadejev@gmail.com>
2020-02-27 23:07:51 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Fragment } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Default from './components/Default';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Modal from './components/Modal';
+
+function App() {
+  return (
+    <Fragment>
+      <Navbar />
+      <Switch>
+        <Route exact path='/' component={ProductList}></Route>
+        <Route path='/details' component={Details}></Route>
+        <Route path='/cart' component={Cart}></Route>
+        <Route component={Default}></Route>
+      </Switch>
+      <Modal />
+    </Fragment>
+  );
+}
+
+export default App;
